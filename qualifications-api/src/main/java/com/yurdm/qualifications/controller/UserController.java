22504d8aa2fe6040ca104b22d453957401a1ce4f
yurdmitriev <yurdmytriiev@gmail.com>
2023-04-18 20:37:38 +0300

    Add API method to get info about User

--- qualifications-api/src/main/java/com/yurdm/qualifications/controller/UserController.java
+++ qualifications-api/src/main/java/com/yurdm/qualifications/controller/UserController.java
@@ -2,6 +2,8 @@ package com.yurdm.qualifications.controller;
 
 import com.yurdm.qualifications.model.users.Student;
 import com.yurdm.qualifications.model.users.User;
+import com.yurdm.qualifications.security.UserDetails;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,19 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 
 @RestController
-@RequestMapping("/users/")
+@RequestMapping("/api")
 public class UserController {
-    @GetMapping("test")
-    public User getTestUser() {
-        var user = new Student();
-
-        user.setId(1);
-        user.setFirstName("Петро");
-        user.setLastName("Петров");
-        user.setEmail("petro@mail.ua");
-        user.setCourse(2);
-        user.setCompetencies(new ArrayList<>());
-
-        return user;
+    @GetMapping("/user")
+    public User getUserInfo() {
+        return ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUser();
     }
 }
