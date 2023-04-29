fa288df87bfaa055753377b2560a1a3bfe76ed87
yurdmitriev <yurdmytriiev@gmail.com>
2023-04-30 01:33:50 +0300

    Update content list items to fit dashboard requirements

--- qualifications-web/src/components/content/EventItem.vue
+++ qualifications-web/src/components/content/EventItem.vue
@@ -1,20 +1,35 @@
 <template>
-  <article>
-    <h5>
+  <article :class="{'list-group-item': dashboard}">
+    <h5 class="d-flex align-items-center">
       <RouterLink :to="`/events/${id}`" class="link-dark text-decoration-none"><b>{{ title }}</b></RouterLink>
-      <i class="ms-1" v-if="company"> від {{ company }}</i>
+      <i class="ms-1" v-if="company && !dashboard"> від {{ company }}</i>
+      <span class="ms-auto btn-group btn-group-sm" role="group" aria-label="Basic example" v-if="dashboard">
+        <button type="button" class="btn btn-outline-primary">
+          <EditIcon />
+        </button>
+        <button type="button" class="btn btn-outline-primary">
+          <VisibilityIcon />
+        </button>
+        <button type="button" class="btn btn-outline-danger">
+          <DeleteIcon />
+        </button>
+      </span>
     </h5>
     <p class="mb-1"><i>{{ startAtDate }}</i> <i class="text-secondary">{{ city ?? "Віддалено" }}</i></p>
-    <p>{{ summary }}</p>
+    <p v-if="!dashboard">{{ summary }}</p>
   </article>
 </template>
 
 <script>
 import { months } from "@/services/util";
+import VisibilityIcon from "@/components/icons/VisibilityIcon.vue";
+import DeleteIcon from "@/components/icons/DeleteIcon.vue";
+import EditIcon from "@/components/icons/EditIcon.vue";
 
 export default {
   name: "EventItem",
-  props: ["id", "title", "description", "summary", "company", "startDate", "city"],
+  components: { EditIcon, DeleteIcon, VisibilityIcon },
+  props: ["id", "title", "description", "summary", "company", "startDate", "city", "dashboard"],
   computed: {
     startAtDate() {
       const current = new Date();
--- qualifications-web/src/components/content/VacancyItem.vue
+++ qualifications-web/src/components/content/VacancyItem.vue
@@ -1,28 +1,45 @@
 <template>
-  <article>
+  <article :class="{'list-group-item': dashboard}">
     <h5 class="d-flex align-items-center">
       <span>
         <RouterLink :to="`/vacancies/${id}`" class="link-dark text-decoration-none"><b>{{ title }}</b></RouterLink>
         <i class="ms-1" v-if="company"> в {{ company }}</i>
       </span>
-      <i class="ms-auto salary" v-if="salary"><b>₴{{ salary }}</b></i>
+      <i class="ms-auto salary" v-if="salary && !dashboard"><b>₴{{ salary }}</b></i>
+      <span class="ms-auto btn-group btn-group-sm" role="group" aria-label="Basic example" v-if="dashboard">
+        <button type="button" class="btn btn-outline-primary">
+          <EditIcon />
+        </button>
+        <button type="button" class="btn btn-outline-primary">
+          <VisibilityIcon />
+        </button>
+        <button type="button" class="btn btn-outline-danger">
+          <DeleteIcon />
+        </button>
+      </span>
     </h5>
-    <p class="mb-1"><i>{{ publishedDate }}</i> <i class="text-secondary">{{ city ?? "Віддалено" }}</i></p>
-    <p>{{ summary }}</p>
+    <p class="mb-1"><i>{{ publishedDate }}</i> <i class="text-secondary">{{ city ?? "Віддалено" }}</i>
+      <span v-if="dashboard"><i class="ms-3" v-if="salary"><b>₴{{ salary }}</b></i></span>
+    </p>
+    <p v-if="!dashboard">{{ summary }}</p>
   </article>
 </template>
 
 <script>
 import { months } from "@/services/util";
+import EditIcon from "@/components/icons/EditIcon.vue";
+import VisibilityIcon from "@/components/icons/VisibilityIcon.vue";
+import DeleteIcon from "@/components/icons/DeleteIcon.vue";
 
 export default {
   name: "VacancyItem",
-  props: ["id", "title", "description", "summary", "company", "publishedAt", "city", "salary"],
+  components: { DeleteIcon, VisibilityIcon, EditIcon },
+  props: ["id", "title", "description", "summary", "company", "publishedAt", "city", "salary", "dashboard"],
   computed: {
     publishedDate() {
       const current = new Date();
       const date = new Date(this.publishedAt);
-      let result = `${date.getDate()} ${months[date.getMonth()]}`
+      let result = `${date.getDate()} ${months[date.getMonth()]}`;
       if (current.getFullYear() - date.getFullYear() > 0) {
         result += ` ${date.getFullYear()}`;
       }
